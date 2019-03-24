fb8b7ee2486fc2db61fcc231f5305fb8ba7a101e
Daniel Darabos <darabos.daniel@gmail.com>
2019-03-24 09:59:35 +0100

    Height field.

--- hft.js
+++ hft.js
@@ -99,7 +99,20 @@ const sin = Math.sin;
 const sqrt = Math.sqrt;
 const floor = Math.floor;
 const min = Math.min;
+const max = Math.max;
+const tanh = Math.tanh;
+const pow = Math.pow;
+
 const maps = [
+  {
+    name: 'test',
+    size: [20, 20],
+    capital: [1000, 3000],
+    height: function(i, j, t) {
+      return 1.1 + tanh(hf.u[i][j]);
+    },
+    update(dt) { hf.update(dt); },
+  },
   {
     size: [2, 1],
     startPos: [0, 0],
@@ -115,6 +128,40 @@ const maps = [
     },
   },
 ]
+
+const hf = {
+  get(i, j) {
+    return hf.u[max(0, min(map.size[0] - 1, i))][max(0, min(map.size[1] - 1, j))];
+  },
+  update(dt) {
+    dt = 20;
+    const u = hf.u;
+    const v = hf.v;
+    const g = hf.get;
+    let tu = 0;
+    let tv = 0;
+    for (let i = 0; i < map.size[0]; ++i) {
+      for (let j = 0; j < map.size[1]; ++j) {
+        const f = g(i - 1, j) + g(i + 1, j) + g(i, j - 1) + g(i, j + 1) - 4 * u[i][j];
+        v[i][j] += 0.015 * f * dt;
+        v[i][j] *= pow(0.999, dt);
+        tv += v[i][j];
+      }
+    }
+    for (let i = 0; i < map.size[0]; ++i) {
+      for (let j = 0; j < map.size[1]; ++j) {
+        u[i][j] += v[i][j] * 0.005 * dt;
+        tu += u[i][j];
+      }
+    }
+    for (let i = 0; i < map.size[0]; ++i) {
+      for (let j = 0; j < map.size[1]; ++j) {
+        u[i][j] -= tu / map.size[0] / map.size[1];
+      }
+    }
+  },
+};
+
 for (let i = 0; i < maps.length; ++i) {
   maps[i].index = i;
 }
@@ -122,8 +169,8 @@ let map;
 let stocks = [];
 const player = { obj: addPlayer() };
 
-function setMap(i) {
-  map = maps[i];
+function setMap(index) {
+  map = maps[index];
   for (let row of stocks) {
     for (let stock of row) {
       scene.remove(stock);
@@ -134,6 +181,19 @@ function setMap(i) {
   player.j = map.startPos ? map.startPos[1] : floor(map.size[1] / 2);
   player.stocks = map.capital[0]
   player.buyPrice = 1;
+
+  hf.u = [];
+  hf.v = [];
+  for (let i = 0; i < map.size[0]; ++i) {
+    const u = [];
+    const v = [];
+    for (let j = 0; j < map.size[1]; ++j) {
+      u.push(0);
+      v.push(0);
+    }
+    hf.u.push(u);
+    hf.v.push(v);
+  }
 }
 setMap(0);
 
@@ -149,6 +209,7 @@ let t;
 function animate(timestamp) {
 	requestAnimationFrame(animate);
   if (startTime === undefined) { startTime = timestamp; }
+  const dt = timestamp - startTime - t || 0;
   t = timestamp - startTime;
   for (let i = 0; i < map.size[0]; ++i) {
     for (let j = 0; j < map.size[1]; ++j) {
@@ -164,6 +225,10 @@ function animate(timestamp) {
   for (let e of effects) {
     e.update(t);
   }
+  map.update && map.update(dt);
+  // TODO: player weight?
+  // hf.u[player.i][player.j] = -10;
+  // hf.v[player.i][player.j] = 0;
 	renderer.render(scene, camera);
   player.capital = floor(player.stocks * map.height(player.i, player.j, t));
   showCapital();
@@ -198,16 +263,18 @@ function addBoom(i, j, gain) {
 
 function onKeyDown(evt) {
   const {i, j} = player;
-  if (evt.key === 'ArrowLeft') {
+  if (evt.key === 'ArrowLeft' && player.i > 0) {
     player.i -= 1;
-  } else if (evt.key === 'ArrowRight') {
+  } else if (evt.key === 'ArrowRight' && player.i < map.size[0] - 1) {
     player.i += 1;
-  } else if (evt.key === 'ArrowUp') {
+  } else if (evt.key === 'ArrowUp' && player.j > 0) {
     player.j -= 1;
-  } else if (evt.key === 'ArrowDown') {
+  } else if (evt.key === 'ArrowDown' && player.j < map.size[1] - 1) {
     player.j += 1;
   } else if (evt.key === 'c' && map.capital[1] <= player.capital) {
     setMap(map.index + 1);
+  } else if (evt.key === ' ') {
+    hf.u[player.i][player.j] = -10;
   }
   if (i !== player.i || j !== player.j) {
     const h0 = map.height(i, j, t);
